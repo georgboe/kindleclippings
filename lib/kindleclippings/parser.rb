fb9912cfc983a52fe9d5d929e062819c17cfb34a
Christophe Philemotte <christophe.philemotte@8thcolor.com>
2013-11-29 11:24:11 +0100

    Fix Regexp to parse page numbering d-d

--- lib/kindleclippings/parser.rb
+++ lib/kindleclippings/parser.rb
@@ -1,44 +1,44 @@
 # encoding: utf-8
 module KindleClippings
-  
+
   class Parser
-    
+
     def parse_file(path)
       file_content = open(path, 'r:utf-8').read
-      
+
       parse(file_content)
     end
-    
+
     def parse(filecontent)
       @clippings = ClippingResult.new
-      
+
       filecontent.split("=" * 10).each do |clipping|
-        
+
         a_clipping = parse_clipping(clipping)
-        
+
         if a_clipping
           @clippings << a_clipping
         end
-        
-        
+
+
       end
       @clippings
     end
-    
+
     private
-    
+
     def parse_clipping(clipping)
       clipping.lstrip!
-      
+
       lines = clipping.lines.to_a
-      
+
       if lines.length < 4
         return nil
       end
-      
+
       first_line = lines[0].strip.scan(/^(.+) \((.+)\)$/i).first
-      second_line = lines[1].strip.scan(/^-\s(?:Your\s)?(\w+) (?:on page (\d+) \| |)Loc(?:ation)?\.? ([0-9-]*?) +\| Added on (.+)$/i).first
-      
+      second_line = lines[1].strip.scan(/^-\s(?:Your\s)?(\w+) (?:on page ([0-9-]*?) \| )?(?:Loc(?:ation|\.) ([0-9-]*?) +\| )?Added on (.*)$/i).first
+
       if first_line.nil?
         title = lines[0].strip
         author = ""
@@ -46,9 +46,9 @@ module KindleClippings
         title, author = *first_line
       end
       type, page, location, date = *second_line
-      
+
       content = lines[3..-1].join("")
-      
+
       Clipping.new(title, author, type.to_sym, location, date, content.strip, page)
     end
   end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,7 +2,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Parser" do
-  
+
   before(:each) do
     @parser = KindleClippings::Parser.new
   end
@@ -38,21 +38,21 @@ Freakonomics Rev Ed (Steven D., Levitt)
 
 In the 1970s, if you were the sort of person who did drugs, there was no classier drug than cocaine. Beloved by rock stars and movie stars, ballplayers and even the occasional politician, cocaine was a drug of power and panache. It was clean, it was white, it was pretty. Heroin was droopy and pot was foggy but cocaine provided a beautiful high.
 EOF
-    
+
     result = @parser.parse(input)
     result.should_not be(nil)
     result.length.should eql(3)
-    
+
   end
-  
+
   it "should parse a 'My Clippings.txt' file" do
     result = @parser.parse_file(File.expand_path(File.dirname(__FILE__) + '/My Clippings.txt'))
     result.should_not be(nil)
-    result.length.should eql(103)
+    result.length.should eql(105)
   end
-  
+
   it "should parse a highlight" do
-    
+
     input =<<EOF
 Last Words (George Carlin)
 - Highlight Loc. 2239-40 | Added on Wednesday, December 02, 2009, 03:55 PM
@@ -60,7 +60,7 @@ Last Words (George Carlin)
 Sometimes they say shoot. But they can’t kid me. Shoot is shit with two o’s.
 EOF
     highlight = @parser.send(:parse_clipping, input)
-    
+
     highlight.should_not be(nil)
     highlight.book_title.should eql('Last Words')
     highlight.author.should eql('George Carlin')
@@ -68,9 +68,9 @@ EOF
     highlight.added_on.should eql(DateTime.new(2009, 12, 02, 15, 55))
     highlight.content.should eql('Sometimes they say shoot. But they can’t kid me. Shoot is shit with two o’s.')
   end
-  
+
   it "should parse a bookmark" do
-    
+
     input =<<EOF
 The Tipping Point: How Little Things Can Make a Big Difference (Malcolm Gladwell)
 - Bookmark Loc. 1933 | Added on Wednesday, December 23, 2009, 09:37 PM
@@ -78,7 +78,7 @@ The Tipping Point: How Little Things Can Make a Big Difference (Malcolm Gladwell
 
 EOF
     bookmark = @parser.send(:parse_clipping, input)
-    
+
     bookmark.should_not be(nil)
     bookmark.book_title.should eql('The Tipping Point: How Little Things Can Make a Big Difference')
     bookmark.author.should eql('Malcolm Gladwell')
@@ -86,9 +86,9 @@ EOF
     bookmark.added_on.should eql(DateTime.new(2009, 12, 23, 21, 37))
     bookmark.content.should eql("")
   end
-  
+
   it "should parse a note" do
-    
+
     input =<<EOF
 The Tipping Point: How Little Things Can Make a Big Difference (Malcolm Gladwell)
 - Note Loc. 1933 | Added on Wednesday, December 23, 2009, 09:37 PM
@@ -98,9 +98,9 @@ world hello
 hello world
 world hello
 EOF
-    
+
     note = @parser.send(:parse_clipping, input)
-    
+
     note.should_not be(nil)
     note.book_title.should eql('The Tipping Point: How Little Things Can Make a Big Difference')
     note.author.should eql('Malcolm Gladwell')
@@ -108,9 +108,9 @@ EOF
     note.added_on.should eql(DateTime.new(2009, 12, 23, 21, 37))
     note.content.should eql("hello world\nworld hello\nhello world\nworld hello")
   end
-  
+
   it "should should handle multiline annotations" do
-    
+
     input =<<EOF
 The Tipping Point: How Little Things Can Make a Big Difference (Malcolm Gladwell)
 - Bookmark Loc. 1933 | Added on Wednesday, December 23, 2009, 09:37 PM
@@ -121,15 +121,15 @@ Ut mauris turpis, cursus non interdum quis, porta vel augue. Pellentesque id orn
 
 Integer ut eros ac tellus egestas tempus placerat et arcu. Nullam porttitor est eu purus commodo tempor. Nullam varius, lacus ut porttitor fringilla, nibh turpis molestie urna, at mattis quam felis sit amet libero. Duis orci sapien, tempus nec mattis id, congue sed turpis. Aenean accumsan, lectus in malesuada blandit, felis nisl consectetur lacus, sed vehicula urna diam placerat neque. Nulla porta orci at ante elementum commodo quis vulputate neque. Aliquam quis leo sit amet lacus mollis egestas. Vivamus ac massa nec felis consectetur tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla facilisi. Nullam non dolor nisl, eu pretium est.
 EOF
-    
+
     clipping = @parser.send(:parse_clipping, input)
-    
+
     clipping.content.should eql("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut hendrerit est semper erat fringilla aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed vel urna augue, et fermentum ante. Maecenas eget arcu id eros consectetur vulputate. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus ut sollicitudin orci. Donec in urna lacinia urna sodales posuere sit amet ut nulla. Duis quis quam et dolor dignissim accumsan. Vestibulum porttitor elit at dolor euismod dapibus scelerisque lacus porta. In vel egestas nunc. Curabitur dapibus iaculis enim eu mattis. Pellentesque quis justo eget ligula congue sagittis sit amet sit amet lorem. Ut augue eros, laoreet consectetur sagittis id, luctus quis dolor. Mauris porta, mauris sed gravida varius, enim enim iaculis felis, vitae blandit arcu orci sed nibh. Proin volutpat lobortis metus a dapibus. Fusce ac quam cursus lorem consequat aliquet. Nulla odio lectus, eleifend sit amet eleifend ac, eleifend quis diam. Mauris malesuada, odio eget hendrerit rhoncus, lectus odio laoreet arcu, ac mattis mi ante fringilla nibh.\n\nUt mauris turpis, cursus non interdum quis, porta vel augue. Pellentesque id ornare ipsum. Morbi vestibulum, purus id consequat molestie, eros lacus congue leo, a lacinia lectus velit nec augue. Aliquam erat volutpat. Cras consectetur porttitor leo sit amet pulvinar. Nunc dignissim justo non mauris eleifend tempus vel a augue. Curabitur accumsan, dui consequat consequat rhoncus, enim nisl lobortis neque, non placerat nulla sem ut leo. Vivamus turpis lacus, rhoncus vitae pellentesque ac, dignissim non lorem. Praesent ullamcorper nisi suscipit mi ornare suscipit. Maecenas pharetra urna quis ante vulputate ac congue lacus hendrerit. Nunc nunc lorem, convallis eget mollis sit amet, laoreet eu nunc.\n\nInteger ut eros ac tellus egestas tempus placerat et arcu. Nullam porttitor est eu purus commodo tempor. Nullam varius, lacus ut porttitor fringilla, nibh turpis molestie urna, at mattis quam felis sit amet libero. Duis orci sapien, tempus nec mattis id, congue sed turpis. Aenean accumsan, lectus in malesuada blandit, felis nisl consectetur lacus, sed vehicula urna diam placerat neque. Nulla porta orci at ante elementum commodo quis vulputate neque. Aliquam quis leo sit amet lacus mollis egestas. Vivamus ac massa nec felis consectetur tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla facilisi. Nullam non dolor nisl, eu pretium est.")
   end
-  
+
   it "should use the entire line as title if no author is available" do
     input =<<EOF
-Static Code Analysis and Code Contracts 
+Static Code Analysis and Code Contracts
 - Note Loc. 1 | Added on Monday, August 29, 2011, 09:06 AM
 
 This is a note.
@@ -140,7 +140,7 @@ EOF
     clipping.book_title.should eql("Static Code Analysis and Code Contracts")
     clipping.author.should eql("")
   end
-  
+
   it "should support page information" do
     input =<<EOF
 Book title (Author)
